f40dfcdbeac80595449ec2cecff069817e20557a
oleg <vinodevmail@gmail.com>
2021-04-07 00:12:20 +0300

    changed url to HTTPS

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const fetchWeather = async (locationKey) => {
 
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${API_KEY}`
+    const url = `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${API_KEY}`
     try {
 
         const res = await fetch(url);
@@ -15,7 +15,7 @@ export const fetchWeather = async (locationKey) => {
 }
 
 export const fetchForecast = async (locationKey) => {
-    const url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${API_KEY}&metric=true`
+    const url = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${API_KEY}&metric=true`
     try {
         const res = await fetch(url);
         const data = await res.json();
@@ -27,7 +27,7 @@ export const fetchForecast = async (locationKey) => {
 }
 
 export const autocompleteSearch = async (query) => {
-    const url = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${query}`
+    const url = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${query}`
     try {
         const res = await fetch(url);
         let data = await res.json();
